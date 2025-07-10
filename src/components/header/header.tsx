5dd786370191dd73124e2f189e62d1f3cd7fa2cc
VaanHovanisian <hovanisian@gmail.com>
2025-07-11 01:33:40 +0400

    update

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,31 +1,27 @@
-"use client"
-import React from 'react';
-import { cn } from '@/lib/utils';
+"use client";
+import React from "react";
+import { cn } from "@/lib/utils";
 import { Burger, Logo, Search, Wallet, Language } from "@/components";
-import { Button } from '../ui';
-import { useTranslations } from 'next-intl';
+import { Button } from "../ui";
+import { useTranslations } from "next-intl";
 
 interface Props {
   className?: string;
 }
 
-
-
 export const Header: React.FC<Props> = (props) => {
   const { className } = props;
-
+  // barev Shant jan
   const t = useTranslations();
 
   return (
     <>
-      <p className='text-center px-4 py-0.5'>
-        {t("deliveryText")}
-      </p>
+      <p className="text-center px-4 py-0.5">{t("deliveryText")}</p>
       <header className={cn("bg-primary-foreground ", className)}>
-        <div className='md:grid md:grid-cols-3 flex justify-between items-center max-w-[85rem] mx-auto py-3 px-4'>
+        <div className="md:grid md:grid-cols-3 flex justify-between items-center max-w-[85rem] mx-auto py-3 px-4">
           <Burger />
-          <Logo className='md:justify-self-center justify-self-start' />
-          <div className='flex items-center gap-3 justify-self-end'>
+          <Logo className="md:justify-self-center justify-self-start" />
+          <div className="flex items-center gap-3 justify-self-end">
             <Search />
             <Wallet />
             <Language
@@ -40,9 +36,8 @@ export const Header: React.FC<Props> = (props) => {
               {t("signOut")}
             </Button>
           </div>
-
         </div>
       </header>
     </>
   );
-}
+};
